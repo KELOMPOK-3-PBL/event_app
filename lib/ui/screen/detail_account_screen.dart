c7a0195adb2f6b114a9947cf4e9da632aede32a8
FaturTuru <affadhika287@gmail.com>
2025-01-01 23:46:01 +0700

    make update roles widget

--- lib/ui/screen/detail_account_screen.dart
+++ lib/ui/screen/detail_account_screen.dart
@@ -1,5 +1,6 @@
 import 'package:event_proposal_app/ui/section/detail_profile_content_section.dart';
 import 'package:event_proposal_app/ui/theme/ui_colors.dart';
+import 'package:event_proposal_app/ui/widget/show_error.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uicons_pro/uicons_pro.dart';
@@ -20,63 +21,290 @@ class _HomeProfile extends State<DetailAccountScreen> {
     super.initState();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
+  List<int> currentRole = [];
+
+  final Map<String, int> roleMapping = {
+    "Member": 1,
+    "Propose": 2,
+    "Admin": 3,
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: UIColor.white,
-      body: BlocBuilder<UserBloc, UserState>(
-        builder: (context, state) {
+      body: BlocConsumer<UserBloc, UserState>(
+        listener: (context, state) {
           if (state is UserByUIDLoaded) {
-            return Column(
-              children: [
-                AppBar(
-                  leading: IconButton(
-                    icon: Icon(
-                      UIconsPro.regularRounded.angle_small_left,
-                      size: 17,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-
-                  automaticallyImplyLeading:
-                      false, // remove leading(left) back icon
-                  centerTitle: true,
-                  backgroundColor: UIColor.solidWhite,
-                  scrolledUnderElevation: 0,
-                  title: Text(
-                    "Account Profile",
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: UIColor.typoBlack,
-                    ),
-                  ),
-                ),
-                DetailProfileContentSection(userData: state.userData),
-              ],
-            );
+            for (var role in state.userData.roles!) {
+              if (!currentRole.contains(roleMapping[role]!)) {
+                // currentRole.add(roleMapping[role]!);
+                currentRole.add(roleMapping[role]!);
+              }
+            }
           }
-          return Center(
-            child: CircularProgressIndicator(),
+        },
+        builder: (context, state) {
+          return BlocBuilder<UserBloc, UserState>(
+            builder: (context, state) {
+              if (state is UserByUIDLoaded) {
+                return Column(
+                  children: [
+                    AppBar(
+                      leading: IconButton(
+                        icon: Icon(
+                          UIconsPro.regularRounded.angle_small_left,
+                          size: 17,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+
+                      automaticallyImplyLeading:
+                          false, // remove leading(left) back icon
+                      centerTitle: true,
+                      backgroundColor: UIColor.solidWhite,
+                      scrolledUnderElevation: 0,
+                      title: Text(
+                        "Account Profile",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: UIColor.typoBlack,
+                        ),
+                      ),
+                    ),
+                    DetailProfileContentSection(userData: state.userData),
+                  ],
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
           );
         },
       ),
       bottomNavigationBar: ButtonUpdateRBACAdmin(
         changeStatus: () {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog();
+          // selectedRoles = currentRole;
+          final List<int> selectedRoles = currentRole;
+
+          showModalBottomSheet(
+            backgroundColor: UIColor.solidWhite,
+            context: context,
+            isScrollControlled: true,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            builder: (BuildContext context) {
+              String? errorMessage;
+              debugPrint("selected ${selectedRoles.toString()}");
+
+              return StatefulBuilder(
+                  builder: (BuildContext context, StateSetter modalSetState) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Change Role',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          CircleAvatar(
+                            radius: 12,
+                            backgroundColor: Colors.grey.shade300,
+                            child: Text(
+                              '${selectedRoles.length}',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 16),
+                      // Display error message if exists
+                      if (errorMessage != null)
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Text(
+                            errorMessage!,
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      Flex(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        direction: Axis.horizontal,
+                        children: roleMapping.keys.map((roleName) {
+                          return _roleButton(
+                            roleName,
+                            selectedRoles.contains(roleMapping[roleName]!)
+                                ? UIColor.getRoleColor(roleName)
+                                : Colors.grey.shade300,
+                            // textColor:
+                            selectedRoles.contains(roleMapping[roleName]!)
+                                ? Colors.white
+                                : Colors.black,
+                            modalSetState,
+                            selectedRoles,
+                          );
+                        }).toList(),
+                        //     roleMapping.keys.map((roleName) {
+                        //   return _roleButton(
+                        //     roleName,
+                        //     selectedRoles.contains(roleMapping[roleName]!)
+                        //         ? Colors.blue
+                        //         : Colors.grey.shade300,
+                        //     textColor:
+                        //         selectedRoles.contains(roleMapping[roleName]!)
+                        //             ? Colors.white
+                        //             : Colors.black,
+                        //   );
+                        // }).toList(),
+                      ),
+                      SizedBox(height: 24),
+                      Flex(
+                        direction: Axis.horizontal,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              // if (selectedRoles.isEmpty) {
+                              //   modalSetState(() {
+                              //     // Simulate another error
+                              //     errorMessage = "Select at least one role.";
+                              //   });
+                              // } else {
+                              Navigator.of(context)
+                                  .pop(); // Close the bottom sheet
+                              // }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              fixedSize: const Size(150, 50),
+                              // padding: const EdgeInsets.symmetric(
+                              //     horizontal: 50, vertical: 20),
+                              backgroundColor: UIColor
+                                  .typoGray2, // Set Cancel button background to grey 200
+                            ),
+                            child: const Text(
+                              'Cancel',
+                              style: TextStyle(
+                                  color: UIColor.typoBlack,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          // _actionButton(
+                          //     'Cancel', Colors.grey.shade300, Colors.black, () {
+                          //   Navigator.pop(context);
+                          // }),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (selectedRoles.isEmpty) {
+                                modalSetState(() {
+                                  // Simulate another error
+                                  errorMessage = "Select at least one role.";
+                                });
+                              } else {
+                                Navigator.of(context)
+                                    .pop(); // Close the bottom sheet
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              fixedSize: const Size(150, 50),
+                              // padding: const EdgeInsets.symmetric(
+                              //     horizontal: 50, vertical: 20),
+                              backgroundColor: Colors
+                                  .blue, // Set Yes, Sign out button background to blue
+                            ),
+                            child: const Text(
+                              'Confirm',
+                              style: TextStyle(
+                                  color: UIColor.solidWhite,
+                                  fontWeight: FontWeight
+                                      .bold), // You might want to change text color to white for better contrast
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
               });
+            },
+          );
         },
       ),
     );
+  }
+
+  Widget _roleButton(String text, Color color, Color textColor,
+      StateSetter modalSetState, List<int> selectedRoles) {
+    return ElevatedButton(
+      onPressed: () {
+        int roleId = roleMapping[text]!;
+        modalSetState(() {
+          if (selectedRoles.contains(roleId)) {
+            selectedRoles.remove(roleId);
+          } else {
+            selectedRoles.add(roleId);
+          }
+        });
+        setState(() {}); // Update the main UI as well
+        debugPrint("selected ${selectedRoles.toString()}");
+        // debugPrint("current ${currentRole.toString()}");
+      },
+      style: ElevatedButton.styleFrom(
+        foregroundColor: textColor,
+        backgroundColor: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  // Widget _actionButton(
+  //     String text, Color backgroundColor, Color textColor, VoidCallback onTap) {
+  //   return ElevatedButton(
+  //     onPressed: onTap,
+  //     style: ElevatedButton.styleFrom(
+  //       foregroundColor: textColor,
+  //       backgroundColor: backgroundColor,
+  //       shape: RoundedRectangleBorder(
+  //         borderRadius: BorderRadius.circular(8),
+  //       ),
+  //       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+  //     ),
+  //     child: Text(
+  //       text,
+  //       style: TextStyle(fontSize: 16),
+  //     ),
+  //   );
+  // }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
--- lib/ui/section/detail_profile_content_section.dart
+++ lib/ui/section/detail_profile_content_section.dart
@@ -11,135 +11,137 @@ class DetailProfileContentSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment:
-            CrossAxisAlignment.center, // Elemen lain tetap di tengah
-        children: [
-          SizedBox(height: 20),
-          CircleAvatar(
-            radius: 60, // Atur ukuran lingkaran di sini
-            backgroundColor: Colors.grey[300],
-            backgroundImage: NetworkImage(userData?.avatarLink ??
-                'https://img.freepik.com/free-vector/illustration-businessman_53876-5856.jpg?t=st=1729955954~exp=1729959554~hmac=21f4e9f848ed4521b47e6041fcd202d019651577ec676e23bba8e7fe93adce16&w=826'), // Ganti dengan URL gambar Anda
-          ),
-
-          const SizedBox(height: 16),
-          Text(
-            // 'Fattur Fadhika',
-            userData?.username ?? 'Tidak Ada Data',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.w800,
+    return Expanded(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.center, // Elemen lain tetap di tengah
+          children: [
+            SizedBox(height: 20),
+            CircleAvatar(
+              radius: 60, // Atur ukuran lingkaran di sini
+              backgroundColor: Colors.grey[300],
+              backgroundImage: NetworkImage(userData?.avatarLink ??
+                  'https://img.freepik.com/free-vector/illustration-businessman_53876-5856.jpg?t=st=1729955954~exp=1729959554~hmac=21f4e9f848ed4521b47e6041fcd202d019651577ec676e23bba8e7fe93adce16&w=826'), // Ganti dengan URL gambar Anda
             ),
-          ),
-          Text(
-            // 'Fattur Fadhika',
-            userData?.email ?? 'Tidak Ada Data',
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          const SizedBox(height: 6),
 
-          Container(
-            margin: const EdgeInsets.only(top: 6, bottom: 30),
-            height: 24,
-            child: ListView.separated(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: userData!.roles!.length,
-              separatorBuilder: (context, index) => const SizedBox(width: 8),
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                  ),
-                  decoration: BoxDecoration(
-                    color: UIColor.getRoleColor(userData!.roles![index]),
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Center(
-                    child: Text(
-                      userData!.roles![index],
-                      style: const TextStyle(
-                        color: UIColor.solidWhite,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400,
+            const SizedBox(height: 16),
+            Text(
+              // 'Fattur Fadhika',
+              userData?.username ?? 'Tidak Ada Data',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            Text(
+              // 'Fattur Fadhika',
+              userData?.email ?? 'Tidak Ada Data',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const SizedBox(height: 6),
+
+            Container(
+              margin: const EdgeInsets.only(top: 6, bottom: 30),
+              height: 24,
+              child: ListView.separated(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: userData!.roles!.length,
+                separatorBuilder: (context, index) => const SizedBox(width: 8),
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      color: UIColor.getRoleColor(userData!.roles![index]),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Center(
+                      child: Text(
+                        userData!.roles![index],
+                        style: const TextStyle(
+                          color: UIColor.solidWhite,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
 
-          // Bagian "About Me" dan "Interests" rata kiri
-          Align(
-            alignment: Alignment.centerLeft, // Rata kiri
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // Rata kiri
-              children: [
-                // Section "About Me"
-                Text(
-                  'About Me',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+            // Bagian "About Me" dan "Interests" rata kiri
+            Align(
+              alignment: Alignment.centerLeft, // Rata kiri
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start, // Rata kiri
+                children: [
+                  // Section "About Me"
+                  Text(
+                    'About Me',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  userData?.about ??
-                      'I am a student with a strong interest in mobile app development, '
-                          'UI/UX design, and gaming. I also enjoy competing in the fields '
-                          'of technology and design, constantly striving to improve my skills.',
-                  textAlign: TextAlign.left, // Teks rata kiri
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
+                  SizedBox(height: 8),
+                  Text(
+                    userData?.about ??
+                        'I am a student with a strong interest in mobile app development, '
+                            'UI/UX design, and gaming. I also enjoy competing in the fields '
+                            'of technology and design, constantly striving to improve my skills.',
+                    textAlign: TextAlign.left, // Teks rata kiri
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
                   ),
-                ),
-                SizedBox(height: 8),
-                // TextButton(
-                //   onPressed: () {},
-                //   child: Text(
-                //     'Read More',
-                //     style: TextStyle(
-                //       color: Colors.blue,
-                //     ),
-                //   ),
-                // ),
-                SizedBox(height: 24),
+                  SizedBox(height: 8),
+                  // TextButton(
+                  //   onPressed: () {},
+                  //   child: Text(
+                  //     'Read More',
+                  //     style: TextStyle(
+                  //       color: Colors.blue,
+                  //     ),
+                  //   ),
+                  // ),
+                  SizedBox(height: 24),
 
-                // Section "Interests"
-                // Text(
-                //   'Interests',
-                //   style: TextStyle(
-                //     fontSize: 18,
-                //     fontWeight: FontWeight.bold,
-                //   ),
-                // ),
-                // SizedBox(height: 16),
-                // Wrap(
-                //   spacing: 8.0,
-                //   runSpacing: 8.0,
-                //   children: [
-                //     InterestChip(label: 'Music'),
-                //     InterestChip(label: 'Workshop'),
-                //     InterestChip(label: 'Art'),
-                //     InterestChip(label: 'Sport'),
-                //     InterestChip(label: 'Food'),
-                //     InterestChip(label: 'Seminar'),
-                //     InterestChip(label: 'E-Sport'),
-                //   ],
-                // ),
-              ],
+                  // Section "Interests"
+                  // Text(
+                  //   'Interests',
+                  //   style: TextStyle(
+                  //     fontSize: 18,
+                  //     fontWeight: FontWeight.bold,
+                  //   ),
+                  // ),
+                  // SizedBox(height: 16),
+                  // Wrap(
+                  //   spacing: 8.0,
+                  //   runSpacing: 8.0,
+                  //   children: [
+                  //     InterestChip(label: 'Music'),
+                  //     InterestChip(label: 'Workshop'),
+                  //     InterestChip(label: 'Art'),
+                  //     InterestChip(label: 'Sport'),
+                  //     InterestChip(label: 'Food'),
+                  //     InterestChip(label: 'Seminar'),
+                  //     InterestChip(label: 'E-Sport'),
+                  //   ],
+                  // ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
